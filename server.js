b5be24fb6cf3d6e8927f74fd52fac5a4f7782a54
Steve <bevets87@gmail.com>
2017-02-16 18:19:16 -0500

    parses header and returns parsedheader data

--- server.js
+++ server.js
@@ -2,14 +2,25 @@
 var express = require('express');
 var app = express();
 
-// middleware
+// functions to parse header
+var parseAcceptLanguage = function(str){
+return str.split(',')[0];
+}
 
+var parseUserAgent = function(str){
+return str.match(/\([^\)]+\)/)[0].slice(1,str.length - 2)
+}
 
+// routes
+app.get('/', function(req,res){
+  var json = {};
 
+  json.language = parseAcceptLanguage(req.headers['accept-language']);
+  json.software = parseUserAgent(req.headers['user-agent']);
+  json.ipaddress = req.connection.remoteAddress;
 
+  res.json(json);
+});
 
-
-
-
-
+// port
 app.listen(process.env.PORT || 3000);
